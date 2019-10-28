40b20dbc931704eb640855eb40ebbf2eefd4f45c
Pavel Baranau <baranovp777@gmail.com>
2019-10-28 12:47:56 +0300

    Update server side project settings;

--- sports-rental-server/src/config.js
+++ sports-rental-server/src/config.js
@@ -1,33 +1,33 @@
 /* eslint-disable no-unused-vars */
-import path from "path";
-import merge from "lodash/merge";
+import path from 'path';
+import merge from 'lodash/merge';
 
 /* istanbul ignore next */
 const requireProcessEnv = name => {
   if (!process.env[name]) {
-    throw new Error("You must set the " + name + " environment variable");
+    throw new Error('You must set the ' + name + ' environment variable');
   }
   return process.env[name];
 };
 
 /* istanbul ignore next */
-if (process.env.NODE_ENV !== "production") {
-  const dotenv = require("dotenv-safe");
+if (process.env.NODE_ENV !== 'production') {
+  const dotenv = require('dotenv-safe');
   dotenv.config({
-    path: path.join(__dirname, "../.env"),
-    sample: path.join(__dirname, "../.env.example")
+    path: path.join(__dirname, '../.env'),
+    sample: path.join(__dirname, '../.env.example')
   });
 }
 
 const config = {
   all: {
-    env: process.env.NODE_ENV || "development",
-    root: path.join(__dirname, ".."),
+    env: process.env.NODE_ENV || 'development',
+    root: path.join(__dirname, '..'),
     port: process.env.PORT || 9000,
-    ip: process.env.IP || "0.0.0.0",
-    apiRoot: process.env.API_ROOT || "",
-    masterKey: requireProcessEnv("MASTER_KEY"),
-    jwtSecret: requireProcessEnv("JWT_SECRET"),
+    ip: process.env.IP || '0.0.0.0',
+    apiRoot: requireProcessEnv('API_ROOT'),
+    masterKey: requireProcessEnv('MASTER_KEY'),
+    jwtSecret: requireProcessEnv('JWT_SECRET'),
     mongo: {
       options: {
         db: {
@@ -45,7 +45,7 @@ const config = {
   test: {},
   development: {
     mongo: {
-      uri: "mongodb://localhost/sports-rental-server-dev",
+      uri: 'mongodb://localhost/sports-rental-server-dev',
       options: {
         debug: true
       }
@@ -55,7 +55,7 @@ const config = {
     ip: process.env.IP || undefined,
     port: process.env.PORT || 8080,
     mongo: {
-      uri: process.env.MONGODB_URI || "mongodb://localhost/sports-rental-server"
+      uri: process.env.MONGODB_URI || 'mongodb://localhost/sports-rental-server'
     }
   }
 };

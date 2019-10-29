5a1ad2fca4a06fc501705f428639a4ab60c0eaff
Pavel Baranau <baranovp777@gmail.com>
2019-10-29 16:58:17 +0300

    Setup run and build scripts;

--- sports-rental-app/src/services/express/index.js
+++ sports-rental-app/src/services/express/index.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import forceSSL from 'express-force-ssl';
+// import forceSSL from 'express-force-ssl';
 import cors from 'cors';
 import compression from 'compression';
 import morgan from 'morgan';
@@ -7,18 +7,19 @@ import bodyParser from 'body-parser';
 import { errorHandler as queryErrorHandler } from 'querymen';
 import { errorHandler as bodyErrorHandler } from 'bodymen';
 import { env } from '../../config';
+import path from 'path';
 
 export default (apiRoot, routes) => {
   const app = express();
 
   /* istanbul ignore next */
-  if (env === 'production') {
-    app.set('forceSSLOptions', {
-      enable301Redirects: false,
-      trustXFPHeader: true
-    });
-    app.use(forceSSL);
-  }
+  // if (env === 'production') {
+  //   app.set('forceSSLOptions', {
+  //     enable301Redirects: false,
+  //     trustXFPHeader: true
+  //   });
+  //   app.use(forceSSL);
+  // }
 
   /* istanbul ignore next */
   if (env === 'production' || env === 'development') {
@@ -26,6 +27,11 @@ export default (apiRoot, routes) => {
     app.use(compression());
     app.use(morgan('dev'));
   }
+
+  app.use(express.static(path.resolve(__dirname, '../../../client/build')));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../../../client/build/index.html'));
+  });
 
   app.use(bodyParser.urlencoded({ extended: false }));
   app.use(bodyParser.json());

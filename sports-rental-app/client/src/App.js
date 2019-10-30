54af590773444a856026713869d29b5b6460b752
Pavel Baranau <baranovp777@gmail.com>
2019-10-30 15:50:59 +0300

    Add Material UI Footer and Header;

--- sports-rental-app/client/src/App.js
+++ sports-rental-app/client/src/App.js
@@ -2,10 +2,14 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import Header from './components/Header';
+import StickyFooter from './components/Footer';
+
 const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
+      <Header />
+      <div className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -13,7 +17,8 @@ const App = () => {
         <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
           Learn React
         </a>
-      </header>
+      </div>
+      <StickyFooter />
     </div>
   );
 };
